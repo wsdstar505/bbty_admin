b4564363ff5cea4625a0732cc2c901d5218b10f7
wwwsd <1137270833@qq.com>
2016-12-24 09:12:16 +0800

    更新表结构和数据

--- DOC/02_数据库表结构/脚本/bbty_data.sql
+++ DOC/02_数据库表结构/脚本/bbty_data.sql
@@ -1,15 +1,20 @@
 prompt PL/SQL Developer import file
-prompt Created on 2016年12月21日 by 翁仕达
+prompt Created on 2016年12月24日 by 翁仕达
 set feedback off
 set define off
 prompt Disabling triggers for T_ROLE...
 alter table T_ROLE disable all triggers;
 prompt Disabling triggers for T_USER...
 alter table T_USER disable all triggers;
+prompt Disabling triggers for T_USER_OPER...
+alter table T_USER_OPER disable all triggers;
 prompt Disabling triggers for T_USER_ROLE...
 alter table T_USER_ROLE disable all triggers;
 prompt Deleting T_USER_ROLE...
 delete from T_USER_ROLE;
+commit;
+prompt Deleting T_USER_OPER...
+delete from T_USER_OPER;
 commit;
 prompt Deleting T_USER...
 delete from T_USER;
@@ -18,36 +23,37 @@ prompt Deleting T_ROLE...
 delete from T_ROLE;
 commit;
 prompt Loading T_ROLE...
-insert into T_ROLE (roleid, rolename)
-values ('admin', '超级管理员');
-insert into T_ROLE (roleid, rolename)
-values ('normal', '普通员工');
-insert into T_ROLE (roleid, rolename)
-values ('admin1', 'sdfsdf');
-insert into T_ROLE (roleid, rolename)
-values ('admin2', 'fsfsdfsdfsd');
+insert into T_ROLE (roleid, rolename, status, remark)
+values ('admin', '超级管理员', '1', null);
+insert into T_ROLE (roleid, rolename, status, remark)
+values ('normal', '普通员工', '1', null);
+insert into T_ROLE (roleid, rolename, status, remark)
+values ('test01', '测试角色1号', '1', '测试用的');
+insert into T_ROLE (roleid, rolename, status, remark)
+values ('test02', '测试角色2号', '0', '停用了');
+insert into T_ROLE (roleid, rolename, status, remark)
+values ('test03', '测试角色3号', '0', '哈哈哈');
 commit;
-prompt 4 records loaded
+prompt 5 records loaded
 prompt Loading T_USER...
-insert into T_USER (empid, username, password)
-values (1, 'wsd', '000000');
-insert into T_USER (empid, username, password)
-values (2, 'sysadmin', '000000');
-commit;
-prompt 2 records loaded
+prompt Table is empty
+prompt Loading T_USER_OPER...
+prompt Table is empty
 prompt Loading T_USER_ROLE...
+insert into T_USER_ROLE (empid, roleid)
+values (1, 'admin');
 insert into T_USER_ROLE (empid, roleid)
 values (1, 'normal');
 insert into T_USER_ROLE (empid, roleid)
 values (2, 'admin');
-insert into T_USER_ROLE (empid, roleid)
-values (1, 'admin');
 commit;
 prompt 3 records loaded
 prompt Enabling triggers for T_ROLE...
 alter table T_ROLE enable all triggers;
 prompt Enabling triggers for T_USER...
 alter table T_USER enable all triggers;
+prompt Enabling triggers for T_USER_OPER...
+alter table T_USER_OPER enable all triggers;
 prompt Enabling triggers for T_USER_ROLE...
 alter table T_USER_ROLE enable all triggers;
 set feedback on
--- DOC/02_数据库表结构/脚本/bbty_table.sql
+++ DOC/02_数据库表结构/脚本/bbty_table.sql
@@ -1,10 +1,10 @@
---------------------------------------------
--- Export file for user BBTY              --
--- Created by 翁仕达 on 2016/12/21, 18:20:56 --
---------------------------------------------
+-------------------------------------------
+-- Export file for user BBTY             --
+-- Created by 翁仕达 on 2016/12/24, 9:11:09 --
+-------------------------------------------
 
 set define off
-spool bbrun_table.log
+spool bbty_table.log
 
 prompt
 prompt Creating table T_ROLE
@@ -13,7 +13,9 @@ prompt
 create table BBTY.T_ROLE
 (
   roleid   VARCHAR2(30) not null,
-  rolename VARCHAR2(255)
+  rolename VARCHAR2(255),
+  status   VARCHAR2(1),
+  remark   VARCHAR2(255)
 )
 tablespace BBTY
   pctfree 10
@@ -32,6 +34,24 @@ comment on column BBTY.T_ROLE.roleid
   is '角色ID';
 comment on column BBTY.T_ROLE.rolename
   is '角色名称';
+comment on column BBTY.T_ROLE.status
+  is '角色状态,（0-停用，1-启用）';
+comment on column BBTY.T_ROLE.remark
+  is '角色备注';
+alter table BBTY.T_ROLE
+  add constraint PK_ROLE primary key (ROLEID)
+  using index 
+  tablespace BBTYIDX
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
 
 prompt
 prompt Creating table T_USER
@@ -39,9 +59,17 @@ prompt =====================
 prompt
 create table BBTY.T_USER
 (
-  empid    NUMBER(10) not null,
-  username VARCHAR2(50),
-  password VARCHAR2(255)
+  empid       NUMBER(10) not null,
+  userid      VARCHAR2(30),
+  username    VARCHAR2(50),
+  gender      VARCHAR2(1),
+  birthdate   VARCHAR2(8),
+  mobileno    VARCHAR2(11),
+  htel        VARCHAR2(12),
+  haddress    VARCHAR2(200),
+  createtime  VARCHAR2(14),
+  lastupttime VARCHAR2(14),
+  remark      VARCHAR2(255)
 )
 tablespace BBTY
   pctfree 10
@@ -58,19 +86,54 @@ comment on table BBTY.T_USER
   is '员工表';
 comment on column BBTY.T_USER.empid
   is '员工号';
+comment on column BBTY.T_USER.userid
+  is '员工登录名';
 comment on column BBTY.T_USER.username
   is '员工姓名';
-comment on column BBTY.T_USER.password
-  is '密码';
+comment on column BBTY.T_USER.gender
+  is '性别,0-女,1-男,2-保密';
+comment on column BBTY.T_USER.birthdate
+  is '出生日期,格式YYYYMMDD';
+comment on column BBTY.T_USER.mobileno
+  is '手机号码,11位';
+comment on column BBTY.T_USER.htel
+  is '家庭电话,区号+号码';
+comment on column BBTY.T_USER.haddress
+  is '家庭地址';
+comment on column BBTY.T_USER.createtime
+  is '创建时间,格式YYYYMMDDHHMMSS';
+comment on column BBTY.T_USER.lastupttime
+  is '最近更新日期,格式YYYYMMDDHHMMSS';
+comment on column BBTY.T_USER.remark
+  is '员工备注';
+alter table BBTY.T_USER
+  add constraint PK_USER primary key (EMPID)
+  using index 
+  tablespace BBTYIDX
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
 
 prompt
-prompt Creating table T_USER_ROLE
+prompt Creating table T_USER_OPER
 prompt ==========================
 prompt
-create table BBTY.T_USER_ROLE
+create table BBTY.T_USER_OPER
 (
-  empid  NUMBER(10),
-  roleid VARCHAR2(30)
+  userid      VARCHAR2(30) not null,
+  password    VARCHAR2(100) not null,
+  status      VARCHAR2(1) not null,
+  lastlogin   VARCHAR2(14) not null,
+  lastupttime VARCHAR2(14) not null,
+  uptempid    NUMBER(10) not null,
+  empid       NUMBER(10) not null
 )
 tablespace BBTY
   pctfree 10
@@ -79,7 +142,50 @@ tablespace BBTY
   storage
   (
     initial 64K
-    next 1M
+    minextents 1
+    maxextents unlimited
+  );
+comment on table BBTY.T_USER_OPER
+  is '操作员表';
+comment on column BBTY.T_USER_OPER.userid
+  is '员工登录名';
+comment on column BBTY.T_USER_OPER.password
+  is '密码';
+comment on column BBTY.T_USER_OPER.status
+  is '操作员状态,（0-停用，1-启用）';
+comment on column BBTY.T_USER_OPER.lastlogin
+  is '最近登录时间,格式YYYYMMDDHHMMSS';
+comment on column BBTY.T_USER_OPER.lastupttime
+  is '最近更新日期,格式YYYYMMDDHHMMSS';
+comment on column BBTY.T_USER_OPER.uptempid
+  is '修改人员';
+comment on column BBTY.T_USER_OPER.empid
+  is '员工号';
+alter table BBTY.T_USER_OPER
+  add constraint PK_USER_OPER primary key (USERID)
+  using index 
+  tablespace BBTYIDX
+  pctfree 10
+  initrans 2
+  maxtrans 255;
+
+prompt
+prompt Creating table T_USER_ROLE
+prompt ==========================
+prompt
+create table BBTY.T_USER_ROLE
+(
+  empid  NUMBER(10) not null,
+  roleid VARCHAR2(30) not null
+)
+tablespace BBTY
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 8K
     minextents 1
     maxextents unlimited
   );
@@ -89,6 +195,20 @@ comment on column BBTY.T_USER_ROLE.empid
   is '员工号';
 comment on column BBTY.T_USER_ROLE.roleid
   is '角色ID';
+alter table BBTY.T_USER_ROLE
+  add constraint PK_USER_ROLE primary key (EMPID, ROLEID)
+  using index 
+  tablespace BBTYIDX
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
 
 
 spool off
